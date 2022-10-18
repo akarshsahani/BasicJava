84ef0efd83f28f3b835894147dcde8f126bdf107
unknown <akarshsahani@gmail.com>
2022-10-18 19:35:23 +0530

    initial commit

---
+++ src/Constructor/Cons.java
@@ -0,0 +1,25 @@
+package Constructor;
+
+public class Cons {
+	
+	int id;
+	int num;
+	String name="Learn";
+	String address;
+	
+//	public Cons() {
+//		System.out.println("invoke");
+//	}
+	
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Cons cons = new Cons();
+		System.out.println(cons.name);
+		
+
+	}
+
+}
